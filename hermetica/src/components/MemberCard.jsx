ca32e7427d20804a35f3eea7ef9e85ff2cae7ff2
akhil683 <akhil.palsra@gmail.com>
2024-01-08 21:34:20 +0530

    added mouse effect to members page

--- hermetica/src/components/MemberCard.jsx
+++ hermetica/src/components/MemberCard.jsx
@@ -7,8 +7,8 @@ const MemberCard = ({ member }) => {
     <div className='flex flex-col max-sm:full'>
               <div className='relative'>
                 <div className='absolute left-2 bottom-0 text-iconBg font-semibold'>
-                  <span className='px-4 py-2 rounded-full bg-subMainText hover:bg-violet hover:text-mainText duration-200'>{name}</span>
-                  <p className='px-4 py-2 mt-4 rounded-full bg-subMainText hover:bg-violet hover:text-mainText duration-200'>{position}</p>
+                  <span className='px-4 py-2 rounded-full bg-mainText hover:bg-violet hover:text-mainText duration-200'>{name}</span>
+                  <p className='px-4 py-2 mt-4 rounded-full bg-mainText hover:bg-violet hover:text-mainText duration-200'>{position}</p>
                 </div>
                 <div className='sm:w-64 w-full sm:h-72 bg-violet rounded-lg overflow-hidden'>
                   <img className='w-full sm:h-full object-cover' src={imageURL} alt="" />
@@ -16,8 +16,8 @@ const MemberCard = ({ member }) => {
               </div>
               <div className='w-56 mt-6 font-semibold max-sm:hidden'>
                 <div className='flex gap-4 text-center'>
-                  <div className='w-10 bg-iconBg border-2 border-subMainText rounded-xl'></div>
-                  <p className='py-2 px-6 rounded-full bg-subMainText text-mainBg'>{name}</p>
+                  <div className='w-10 bg-transparent border-2 border-mainText rounded-full'></div>
+                  <p className='py-2 px-6 rounded-full bg-mainText text-mainBg'>{name}</p>
                 </div>
                 <div className='pt-2 backdrop-blur-md mt-4 bg-iconbgHover rounded-md'>
                     <div className='p-2 bg-iconBg rounded'>
--- hermetica/src/routes/Members.jsx
+++ hermetica/src/routes/Members.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Profile from '../assets/profile.jpg';
 import MemberCard from '../components/MemberCard';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
 
 const Members = () => {
   const Members = [
@@ -30,6 +31,27 @@ const Members = () => {
     },
   ]
 
+  const cursorSize = 20;
+  const mouse = {
+    x: useMotionValue(0),
+    y: useMotionValue(0),
+  }
+  const smoothOptions = { damping: 50, stiffness: 200, mass: 0.2 };
+  const smoothMouse = {
+    x: useSpring(mouse.x, smoothOptions),
+    y: useSpring(mouse.y, smoothOptions),
+  }
+  const manageMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    mouse.x.set(clientX - cursorSize);
+    mouse.y.set(clientY - cursorSize);
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", manageMouseMove)
+    return () => {window.removeEventListener("mousemove",manageMouseMove)}
+  });
+
   return (
     <div className='m-4'>
       <div>
@@ -39,7 +61,22 @@ const Members = () => {
               <MemberCard member={member} key={member.id} />
             )})}
         </div>
-        
+
+        <motion.div 
+          className='w-56 fixed mt-6 font-semibold max-sm:hidden  pointer-events-none'
+          style={{left: smoothMouse.x, top: smoothMouse.y}}
+        >
+                <div className='flex gap-4 text-center'>
+                  <div className='w-10 bg-transparent border-2 border-mainText rounded-full'></div>
+                  <p className='py-2 px-6 rounded-full bg-mainText text-mainBg'>Akhil</p>
+                </div>
+                <div className='pt-2 backdrop-blur-md mt-4 bg-iconbgHover rounded-md'>
+                    <div className='p-2 bg-iconBg rounded'>
+                      <span>Executive Member</span>
+                      <p className=' font-normal text-sm leading-tight text-subMainText text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut accusamus temporibus ratione minus? Natus sit id earum inventore commodi, repellat aliquam dicta pariatur omnis ex aut labore nesciunt consetur ut?</p>
+                    </div>
+                </div>
+              </motion.div>
       </div>
     </div>
   )
